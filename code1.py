96a1525fff7eea95e0376782bc3b72fedcaa325b
thawatchairst <minirst26@gmail.com>
2021-07-07 21:57:58 +0700

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,16 @@
+from flask import request
+
+@app.route('/')
+def index():
+    module = request.args.get("module")
+    exec("import urllib%d as urllib" % int(module)
+	
+@app.route('/2')
+def index():
+    module = request.args.get("module")
+    exec("import urllib%d as urllib" % int(module)
+	
+@app.route('/3')
+def index():
+    module = int(request.args.get("module"))
+    exec("import urllib%d as urllib" % int(module)
