105fa912229d6c1c7000b8d6025176e63b5ede36
thawatchairst <minirst26@gmail.com>
2021-07-07 22:08:31 +0700

    Update code2.py

--- code2.py
+++ code2.py
@@ -6,11 +6,11 @@ def index():
     exec("import urllib%d as urllib" % module
 	
 @app.route('/2')
-def index():
+def index2():
     module = request.args.get("module")
     exec("import urllib%d as urllib" % int(module)
 	
 @app.route('/3')
-def index():
+def index3():
     module = int(request.args.get("module"))
     exec("import urllib%d as urllib" % module
