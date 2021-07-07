ca08ee9ebe9392bd1f518894b25c782d52c3f954
thawatchairst <minirst26@gmail.com>
2021-07-07 22:24:27 +0700

    Update code4.py

--- code4.py
+++ code4.py
@@ -3,4 +3,4 @@ from flask import request
 @app.route('/')
 def index():
     module = int(request.args.get("module"))
-    exec("import urllib%d d as urllib" % modlue) 
+    exec("import urllib%d d as urllib" % module) 
