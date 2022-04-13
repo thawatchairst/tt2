79b20114a406f37b77eeb739f03d0a9c5199c322
thawatchairst <minirst26@gmail.com>
2022-04-13 18:47:59 +0700

    Delete P-23.py

--- P-23.py
+++
@@ -1,4 +0,0 @@
-import crypt
-from hashlib import pbkdf2_hmac
-s = b'D8VxSmTZt2E2YV454mkqAY5e'
-hash = pbkdf2_hmac('sha256', password, b'D8VxSmTZt2E2YV454mkqAY5e', 100000)    # Noncompliant: salt is hardcoded
